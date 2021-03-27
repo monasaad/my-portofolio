c8e8e89ab98ad1ab9e2281e57001a1f4d8d19fdb
Mona <monaaa.saad@yahoo.com>
2021-03-28 01:04:37 +0300

    1

--- src/Main.js
+++ src/Main.js
@@ -2,7 +2,7 @@ function Main(){
     return(
         <section id="brief" style={{marginTop: "250px", marginBottom: "150px"}}>
             <h1>Hi, I'm Mona 😊</h1>
-            <h1>UI/UX designer, </h1>
+            <h1>UI and UX designer, </h1>
             <h1>Front End Developer</h1>
             <h1>Based in Saudi Arabia</h1>
         </section>
